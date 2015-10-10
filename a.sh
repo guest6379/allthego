d4cc40eb260f6c81ffe3e299e9425331dfa86e54
guest6379 <guest6379@users.noreply.github.com>
2015-10-10 13:07:46 +0000

    commit 40

---
+++ a.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+for ((i=10;i<=40;i++))
+do
+	echo "i=$i"
+	echo "**$(date)**" >> README.md
+	echo "" >> README.md
+	git commit -am "commit ${i}" && \
+	git push -u origin master
+done
