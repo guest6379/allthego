5391003e229fc131d63dea90bf77eccc215a5c45
guest6379 <guest6379@users.noreply.github.com>
2015-10-10 13:57:16 +0000

    commit 10

--- a.sh
+++ a.sh
@@ -3,7 +3,7 @@
 for ((i=10;i<=40;i++))
 do
 	echo "i=$i"
-	echo "**$(date)**" >> README.md
+	echo "**$(date +'%F %T')**" >> README.md
 	echo "" >> README.md
 	git commit -am "commit ${i}" && \
 	git push -u origin master
